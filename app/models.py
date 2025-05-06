0b82b058469decde2c004003ef60793d11f9893b
Rafaelso-155-lem <rafaelcampanelliterra@gmail.com>
2025-05-06 10:03:49 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -12,8 +12,7 @@ class Cidade(models.Model):
 
 class Autor(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome do autor")
-    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE,
-verbose_name="Cidade do autor")
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE,verbose_name="Cidade do autor")
     def __str__(self):
         return self.nome
     class Meta:
@@ -25,8 +24,7 @@ class Editora(models.Model):
 
     site = models.CharField(max_length=100, verbose_name="Site da editora")
 
-    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE,
-verbose_name="Cidade da editora")
+    cidade = models.ForeignKey(Cidade, on_delete=models.CASCADE,verbose_name="Cidade da editora")
     def __str__(self):
         return self.nome
     class Meta:
@@ -38,8 +36,7 @@ class Leitor(models.Model):
 
     email = models.CharField(max_length=100, verbose_name="Email do leitor")
 
-    cpf = models.CharField(max_length=11, unique=True,
-verbose_name="CPF do leitor")
+    cpf = models.CharField(max_length=11, unique=True,verbose_name="CPF do leitor")
     def __str__(self):
         return self.nome
     class Meta:
@@ -56,16 +53,11 @@ class Genero(models.Model):
 
 class Livro(models.Model):
     nome = models.CharField(max_length=100, verbose_name="Nome do livro")
-
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE,
-verbose_name="Autor do livro")
-    editora = models.ForeignKey(Editora, on_delete=models.CASCADE,
-verbose_name="Editora do livro")
-    genero = models.ForeignKey(Genero, on_delete=models.CASCADE,
-verbose_name="Gênero do livro")
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE,verbose_name="Autor do livro")
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE,verbose_name="Editora do livro")
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE,verbose_name="Gênero do livro")
     preco = models.IntegerField(verbose_name="Preço do livro")
     data_plub = models.DateField(verbose_name="Data de publicação do livro")
-
     status = models.BooleanField(verbose_name="Status do livro")
 
     def __str__(self):
@@ -73,5 +65,30 @@ verbose_name="Gênero do livro")
     class Meta:
         verbose_name = "Livro"
         verbose_name_plural = "Livros"
+        
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=100,verbose_name="Gênero")
+    def __str__(self):
+        return self.nome
+    
+class Meta:
+    verbose_name = "Gênero"
+    verbose_name_plural = "Gêneros"
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome do livro")
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE,verbose_name="Autor do livro")
+    editora = models.ForeignKey(Editora,on_delete=models.CASCADE, verbose_name="Editora do livro")
+    genero = models.ForeignKey(Genero,on_delete=models.CASCADE,verbose_name="Gênero do livro")
+    preco = models.IntegerField(verbose_name="Preço do livro")
+    data_plub = models.DateField(verbose_name="Data de publicação do livro")
+    status = models.BooleanField(verbose_name="Status do livro")
+
+def __str__(self):
+    return f'{self.nome}, {self.autor}'
+class Meta:
+    verbose_name = "Livro"
+    verbose_name_plural = "Livros"
 
 
--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,14 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('', IndexView.as_view(), name='index'),
+
+
     path('livros/', LivrosView.as_view(), name='livros'),
+
     #path('reserva/', EmprestimoView.as_view(), name='reserva'),
+
     path('cidade/', CidadesView.as_view(), name='cidade'),
 
     path('autor/', AutoresView.as_view(), name='autor'),
@@ -17,4 +22,12 @@ urlpatterns = [
     path('leitor/', LeitoresView.as_view(), name='leitor'),
 
     path('genero/', GenerosView.as_view(), name='genero'),
+
+    path('delete/<int:id>/', DeleteLivroView.as_view(), name='delete'),
+
+    # path('', ConsultaView.as_view(), name='livros'),
+
+    #path('reserva/', ReservaView.as_view(), name='reserva'),
+
+    
 ]
